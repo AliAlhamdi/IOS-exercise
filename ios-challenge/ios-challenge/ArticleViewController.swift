ada402a4646a0a694b865ee6448d05130bf9679a
lilou212 <ali-alhamdi@outlook.com>
2018-05-01 17:58:43 +0300

    get data from API and view it, but the details can't show yet neither image

---
+++ ios-challenge/ios-challenge/ArticleViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ArticleViewController.swift
+//  ios-challenge
+//
+//  Created by Ali Hamdi on 30/04/2018.
+//  Copyright © 2018 Ali Hamdi. All rights reserved.
+//
+
+import UIKit
+
+/* an extension from https://stackoverflow.com/questions/24231680/loading-downloading-image-from-url-on-swift
+ that let download images from the web
+ */
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
+
+class ArticleViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var descriptionView: UILabel!
+    
+    var art: Article?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleView.text = art?.title
+        descriptionView.text = art?.content
+        let urlString = (art?.image_url)!
+        let url = URL(string: urlString)
+        imageView.downloadedFrom(url: url!)
+        
+    }
+
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ios-challenge/ios-challenge/ViewController.swift
+++ ios-challenge/ios-challenge/ViewController.swift
@@ -8,18 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct iosEx: Decodable{
+    let title: String
+    let articles: [Article]
+}
+struct Article: Decodable{
+    let title: String
+    let content: String
+    let image_url: String
+}
 
+var refresher : UIRefreshControl!
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var articlesTV: UITableView!
+    
+    var articles =  [Article]()
+//    var exercise = iosEx()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        articlesTV.rowHeight = UITableViewAutomaticDimension
+        fetchJSON ()
+        
+        articlesTV.delegate = self
+        articlesTV.dataSource = self
+        
+        articlesTV.estimatedRowHeight = 50
+        articlesTV.rowHeight = UITableViewAutomaticDimension
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = articlesTV.dequeueReusableCell(withIdentifier: "cell")
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = articles[indexPath.row].title
+        cell.detailTextLabel?.text = articles[indexPath.row].content
+        cell.imageView?.downloadedFrom(link: articles[indexPath.row].image_url)
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetails", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "IOS"
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? ArticleViewController{
+            destination.art = articles[(articlesTV.indexPathForSelectedRow?.row)!]
+        }
+    }    
+    
+    // function to download the JSON request.
+    
+    func fetchJSON(){
+        let json_url = "https://no89n3nc7b.execute-api.ap-southeast-1.amazonaws.com/staging/exercise"
+        guard let url  = URL(string: json_url) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else{ return }
+            if error == nil{
+                do{
+                    let content = try JSONDecoder().decode(iosEx.self, from: data)
+                    self.articles = content.articles
+                    DispatchQueue.main.async {
+                        self.articlesTV.reloadData()
+                    }
+                }catch {
+                    print("JSON Error!")
+                }
+            }
+        }.resume()
+    
+    }
+    
+    
 
 }
 
